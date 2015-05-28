4a73c5417a3525ded11a870260d02aef0ceb65d9
WonYoung <dnjsdud0105@naver.com>
2015-05-28 12:07:34 +0900

    xml파일 추가 및 경로 수정

---
+++ Refrigerator/php/BarcodeInsert.php
@@ -0,0 +1,14 @@
+<?php
+$connect = mysqli_connect ( "mysql.hostinger.kr", "u291047931_admin", "fm8ldlNY4A", "u291047931_db" ) or die("db접속에러"); // DB가 있는 주소(이것은 웹서버로 직접 접속하는 것이기 때문에 루프백 주소를 써도 됨)
+mysqli_select_db ($connect ,"u291047931_db") or die("DB 선택 에러"); // DB 선택
+mysqli_query ( $connect ,"set names utf8" ); // 이것 또한 한글(utf8)을 지원하기 위한 것
+
+$type = $_REQUEST [type];
+$code = $_REQUEST [code];
+$name = $_REQUEST [name];
+$expirydate = $_REQUEST [expirydate];
+
+$qry = "insert into BarcodeInfo (Type, Code, Name, ExpiryDate) values ('$type', $code, '$name', $expirydate)";
+$result = mysqli_query ( $connect, $qry );
+
+?>
---
+++ Refrigerator/src/com/example/refrigerator/LoginActivity.java
@@ -0,0 +1,65 @@
+package com.example.refrigerator;
+
+import com.example.refrigerator.util.SystemUiHider;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ *
+ * @see SystemUiHider
+ */
+public class LoginActivity extends Activity {
+	/**
+	 * Whether or not the system UI should be auto-hidden after
+	 * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+	 */
+	private static final boolean AUTO_HIDE = true;
+
+	/**
+	 * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
+	 * user interaction before hiding the system UI.
+	 */
+	private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+
+	/**
+	 * If set, will toggle the system UI visibility upon interaction. Otherwise,
+	 * will show the system UI visibility upon interaction.
+	 */
+	private static final boolean TOGGLE_ON_CLICK = true;
+
+	/**
+	 * The flags to pass to {@link SystemUiHider#getInstance}.
+	 */
+	private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
+
+	/**
+	 * The instance of the {@link SystemUiHider} for this activity.
+	 */
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_login);
+		// Upon interacting with UI controls, delay any scheduled hide()
+		// operations to prevent the jarring behavior of controls going away
+		// while interacting with the UI.
+		Handler handler = new Handler(){
+			@Override
+			public void handleMessage(Message msg) {
+				finish();
+			}
+		};
+		handler.sendEmptyMessageDelayed(0, 6000);
+	}
+
+}
